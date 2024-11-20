b688d8664ef33f8a3ca682498655b0b74e6c210b
Yuno <yuno@yunos-mbp.speedport.ip>
2024-11-20 23:41:16 +0100

    netlify stuff

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/easypz' : '', // Replace 'easypz' with your repo name
   images: {
     unoptimized: true,
   },
