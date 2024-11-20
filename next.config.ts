7792639afbdec3b83a1546766e8c2160d0ada857
Yuno <yuno@yunos-mbp.speedport.ip>
2024-11-20 23:26:36 +0100

    github publishing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/easypz' : '', // Replace 'easypz' with your repo name
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
